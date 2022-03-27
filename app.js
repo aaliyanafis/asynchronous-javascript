e805d2bfce7c4e73187905bcb31cfe76b0f35d41
Aaliya Nafis <aliyajavascript@gmail.com>
2022-03-27 17:32:42 +0530

    Fetch API

--- app.js
+++ app.js
@@ -3,25 +3,54 @@
 let p = new Promise((resolve, reject) => {
 
     let a = 1 + 6
-    
+
     if (a == 2) {
-    
-    resolve('Success')
-    
+
+        resolve('Success')
+
     } else {
-    
-    reject('Failed')
-    
+
+        reject('Failed')
+
     }
-    
-    })
-    
-    p.then((message) => {
-    
+
+})
+
+p.then((message) => {
+
     console.log('This is in the then ' + message)
-    
-    }).catch((message) => {
-    
+
+}).catch((message) => {
+
     console.log('This is in the catch ' + message)
-    
+
+})
+
+
+//Fetch API
+
+fetch('https://reqres.in/api/users', {
+
+    method: 'POST',
+
+    headers: {
+
+        'Content-Type': 'application/json'
+
+    },
+
+    body: JSON.stringify({
+
+        name: 'User 1'
+
     })
+
+}).then(res => {
+
+    return res.json()
+
+})
+
+    .then(data => console.log(data))
+
+    .catch(error => console.log('ERROR'))
